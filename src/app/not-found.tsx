a70c8de7ea6a8d3d213ad97b07c80b73d681e8db
Can Usta <can.usta@common.studio>
2026-07-07 19:50:17 +0100

    -

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center px-6 py-24">
-      <p className="text-sm text-muted">Not found</p>
+      <p className="text-sm text-muted">:)</p>
     </div>
   );
 }
